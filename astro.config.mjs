82d8e86d7b6057f06b3b173c6f2eef309923da63
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-09-18 16:25:50 +0800

    config: Add github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import partytown from "@astrojs/partytown";
 
+const gethubUsername = 'INSERT_GITHUB_USERNAME_HERE';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [partytown()],
+  site: `https://${githubUsername}.github.io`,
 });
