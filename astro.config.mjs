a5d8df6cd3350835f43c2283284de77b0460a7e7
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-09-18 16:50:54 +0800

    Apply robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import partytown from "@astrojs/partytown";
+import robotsTxt from "astro-robots-txt";
 
 const githubUsername = import.meta.env.GITHUB_REPOSITORY_OWNER || 'INSERT_GITHUB_USERNAME_HERE';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()],
-  site: `https://${githubUsername}.github.io`,
+  integrations: [
+    partytown(),
+    robotsTxt({
+      sitemap: false,
+      policy: [
+        {
+          userAgent: '*',
+          disallow: ['/'],
+        }
+      ],
+    }),
+  ],
+  site: `https://${githubUsername}.github.io`
 });
