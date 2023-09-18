b368186cdfb6bd7860828a00b77d17a05e15322a
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-09-18 16:39:39 +0800

    fix github config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import partytown from "@astrojs/partytown";
 
-const githubUsername = 'INSERT_GITHUB_USERNAME_HERE';
+const githubUsername = import.meta.env.GITHUB_REPOSITORY_OWNER || 'INSERT_GITHUB_USERNAME_HERE';
 
 // https://astro.build/config
 export default defineConfig({
