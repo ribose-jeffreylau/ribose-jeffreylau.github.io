adb7ccf8020821a404f7cf45d356a5f9d6fc0136
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-09-22 15:31:54 +0800

    Activate prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
-
+import prefetch from "@astrojs/prefetch";
 const githubUsername = import.meta.env.GITHUB_REPOSITORY_OWNER || 'INSERT_GITHUB_USERNAME_HERE';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
         }
       ],
     }),
+    prefetch(),
   ],
   site: `https://${githubUsername}.github.io`
 });
