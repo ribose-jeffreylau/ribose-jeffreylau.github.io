1912d321aacd94e732558a567c5672913a53f0ff
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-09-25 16:21:12 +0800

    Add service worker integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 import prefetch from "@astrojs/prefetch";
+import serviceWorker from "astrojs-service-worker";
+
 const githubUsername = import.meta.env.GITHUB_REPOSITORY_OWNER || 'INSERT_GITHUB_USERNAME_HERE';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    serviceWorker(),
     partytown(),
     robotsTxt({
       sitemap: false,
